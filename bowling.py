37282f299584d47d153bbcdcee89e11d57b31cff
Bende Hajdu <outdoorsport85@gmail.com>
2017-11-23 12:01:16 +0100

    simplified "get_value()", changed magic numbers to variable, did auto pep8

--- bowling.py
+++ bowling.py
@@ -1,44 +1,39 @@
 def score(game):
     result = 0
     frame = 1
-    in_first_half = True
+    is_half_try = True
+    pin = 10
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last
+            result += pin - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10 and get_value(game[i]) == 10:
+        if frame < pin and get_value(game[i]) == pin:
             if game[i] == '/':
                 result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == 'x':
                 result += get_value(game[i + 1])
                 if game[i + 2] == '/':
-                    result += 10 - get_value(game[i + 1])
+                    result += pin - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
         last = get_value(game[i])
-        if not in_first_half:
+        if not is_half_try:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
+        if is_half_try:
+            is_half_try = False
         else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
+            is_half_try = True
+        if game[i].lower() == 'x':
+            is_half_try = True
             frame += 1
     return result
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char.isnumeric():
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char.lower() == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
