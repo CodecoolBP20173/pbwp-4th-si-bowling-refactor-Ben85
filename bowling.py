ecbcc9aa59a4f44f2c52ea7dabd5bf71d29fa855
Bende Hajdu <outdoorsport85@gmail.com>
2017-11-26 21:07:10 +0100

    final push. no major changes, rather a security push.

--- bowling.py
+++ bowling.py
@@ -2,22 +2,23 @@ def score(game):
     result = 0
     frame = 1
     is_half_try = True
-    pin = 10
+    PIN = 10
+    FULL_FRAME = 10
     for i in range(len(game)):
         if game[i] == '/':
-            result += pin - last
+            result += PIN - last_try
         else:
             result += get_value(game[i])
-        if frame < pin and get_value(game[i]) == pin:
+        if frame < FULL_FRAME and get_value(game[i]) == PIN:
             if game[i] == '/':
                 result += get_value(game[i + 1])
             elif game[i].lower() == 'x':
                 result += get_value(game[i + 1])
                 if game[i + 2] == '/':
-                    result += pin - get_value(game[i + 1])
+                    result += PIN - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
-        last = get_value(game[i])
+        last_try = get_value(game[i])
         if not is_half_try:
             frame += 1
         if is_half_try:
@@ -31,7 +32,7 @@ def score(game):
 
 
 def get_value(char):
-    try:    
+    try:
         if char.isnumeric():
             return int(char)
         elif char.lower() == 'x' or char == '/':
